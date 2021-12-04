99835d11e871cdbaa06262a70d0a2c1c7e1aa5c5
Rowland Rose <rowlandrose@gmail.com>
2021-12-04 14:40:07 -0500

    Migrations now check toml file

--- framework/migrations.php
+++ framework/migrations.php
@@ -4,6 +4,7 @@ namespace Formfig\Framework;
 
 use DateTime;
 use DateTimeZone;
+use Yosymfony\Toml\Toml;
 
 class Migrations {
 
@@ -26,52 +27,61 @@ class Migrations {
 
         foreach($file_paths as $file_path) {
 
-            if( is_file($file_path) ) {
+            $table_name = "";
+            $ts = 0;
 
-                $file_name = array_slice(explode("/", $file_path), -1)[0];
+            if( !is_file($file_path) ) { continue; } // Is not a valid file page, so skip
 
-                // Only doing "first" if no full migration has been generated yet.
+            $file_name = array_slice(explode("/", $file_path), -1)[0];
 
-                if( strpos($file_name, "-first.toml") ) {
+            // Only doing "first" if no full migration has been generated yet.
 
-                    $table_name = str_replace("-first.toml", "", $file_name);
+            if( strpos($file_name, "-first.toml") ) {
 
-                    if( !is_file($_SERVER["DOCUMENT_ROOT"].'/../local-data/migrations.toml') ) {
+                $table_name = str_replace("-first.toml", "", $file_name);
 
-                        $migration_paths[$table_name][] = $file_path;
+                if( is_file($_SERVER["DOCUMENT_ROOT"].'/../local-data/migrations.toml') ) {
 
-                    } else {
+                    // Read from local-data/migrations.toml to see if there's an entry for this table_name
+                    $md = Toml::ParseFile($_SERVER["DOCUMENT_ROOT"].'/../local-data/migrations.toml');
 
-                        // Read from local-data/migrations.toml to see if there's an entry for this table_name
-
+                    if( isset($md[$table_name]) && !empty($md[$table_name]) ) {
+                        continue; // Migration was run at least once on this table, so don't add 'first'
                     }
-
-                    continue;
-
-                } else {
-
-                    $parts = explode("-", $file_name);
-                    if( count($parts) < 8) {
-                        continue; // Not a valid date-based migration
-                    }
-
-                    // Get timestamp from migration datetime string
-                    $l = count($parts) - 1; // Last index
-                    $date_str = $parts[$l-6]."-".$parts[$l-5]."-".$parts[$l-4];
-                    $time_str = $parts[$l-3].":".$parts[$l-2].":".$parts[$l-1];
-                    $tz_with_day = str_replace(".toml", "", $parts[$l]);
-                    $tz_offset = (int)$tz_with_day - (24*60*60);
-                    $dt = new DateTime($date_str." ".$time_str, new DateTimeZone('GMT'));
-                    $ts = (int)$dt->getTimestamp() + $tz_offset;
-
-                    $table_name = str_replace("-".$parts[$l-6]."-".$parts[$l-5]."-".$parts[$l-4]."-".$parts[$l-3]."-".$parts[$l-2]."-".$parts[$l-1]."-".$parts[$l], "", $file_name);
-
-                    $migration_paths[$table_name][] = $file_path;
                 }
 
-                // Add datetime-based migrations if their datetime is later than last migration
+            } else {
 
+                $parts = explode("-", $file_name);
+
+                if( count($parts) < 8) { continue; } // Not a valid date-based migration
+
+                // Get timestamp from migration datetime string
+                $l = count($parts) - 1; // Last index
+                $date_str = $parts[$l-6]."-".$parts[$l-5]."-".$parts[$l-4];
+                $time_str = $parts[$l-3].":".$parts[$l-2].":".$parts[$l-1];
+                $tz_with_day = str_replace(".toml", "", $parts[$l]);
+                $tz_offset = (int)$tz_with_day - (24*60*60);
+                $dt = new DateTime($date_str." ".$time_str, new DateTimeZone('GMT'));
+                $ts = (int)$dt->getTimestamp() + $tz_offset;
+
+                $table_name = str_replace("-".$parts[$l-6]."-".$parts[$l-5]."-".$parts[$l-4]."-".$parts[$l-3]."-".$parts[$l-2]."-".$parts[$l-1]."-".$parts[$l], "", $file_name);
+
+                // Read from local-data/migrations.toml to see if there's an entry for this table_name
+                $md = Toml::ParseFile($_SERVER["DOCUMENT_ROOT"].'/../local-data/migrations.toml');
+                $last_migration_ts = 0;
+                if( isset($md[$table_name]) && !empty($md[$table_name]) ) {
+                    $last_migration_ts = $md[$table_name];
+                }
+
+                if( $last_migration_ts >= $ts ) { continue; } // Migration already happened so skip.
             }
+
+            $migration_paths[$table_name][] = [
+                "file_path" => $file_path,
+                "table_name" => $table_name,
+                "timestamp" => $ts,
+            ];
         }
 
         echo '<pre>';print_r($migration_paths);echo '</pre>';
